fd391c63f6b14a12b977fb1b48d5184594c6cefc
dharabagadiya <dharabagadiya1990@gmail.com>
2015-09-30 00:24:54 +0530

    test commit

---
+++ EnqiryModule/Controllers/EnquiryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataModel;
+
+namespace EnqiryModule.Controllers
+{
+    public class EnquiryController : BaseController
+    {
+        // GET: Enquiry
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult AddEnquiry(string pincode, string address, string optionOne, string optionMulti, string name, string mobileNumber, string email)
+        {
+            var enquiryManager = new EnquiryManager();
+            var status = enquiryManager.Add(pincode, address, optionOne, optionMulti, name, mobileNumber, email);
+            return Json(status);
+        }
+    }
+}
