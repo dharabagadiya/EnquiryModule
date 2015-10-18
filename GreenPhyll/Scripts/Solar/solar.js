5bb812cd921a5ca418c0ad3ef2f2e986f8a45d0e
dharabagadiya <dharabagadiya1990@gmail.com>
2015-10-18 22:07:18 +0530

    add files for solar and wind

---
+++ GreenPhyll/Scripts/Solar/solar.js
@@ -0,0 +1,35 @@
+﻿var solar = {};
+solar.AddSolarDetail = function () {
+    var solar = {};
+    solar.SolarServiceType = $('.SolarServiceType').find('.selected').find('.label').text();
+    solar.ApplicantType = $('.ApplicantType').find('.selected').find('.label').text();
+    solar.pincode = $("#txtPincode").val();
+    solar.address = $("#txtAddress").val();
+    solar.GPSLatitude = $("#txtGPSLatitude").val();
+    solar.GPSLongitude = $("#txtGPSLongitude").val();
+    solar.MonthlyElectricityBill = $("#txtMonthlyElectricityBill").val();
+    solar.ProposedCapacityKW = $("#txtProposedCapacityKW").val();
+    solar.ShadowFreeArea = $("#txtShadowFreeArea").val();
+    solar.IntallationReqForm = $("#txtIntallationReqForm").val();
+    solar.name = $("#txtName").val();
+    solar.mobileNumber = $("#txtMobileNo").val();
+    solar.email = $("#txtEmailEnquiry1").val();
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        type: "POST",
+        url: "/Solar/Add",
+        async: false,
+        data: JSON.stringify(solar),
+        success: function (data) {
+            var status = data;
+            if (status) {
+                //window.location.href = "Enquiry/ThankYou";
+                alert("Success");
+            } else { }
+        }
+    });
+}
+$(document).ready(function () {
+    $('#btnSolarSubmit').off("click").on("click", function () { solar.AddSolarDetail(); });
+});
---
+++ GreenPhyll/Scripts/Wind/wind.js
@@ -0,0 +1,38 @@
+﻿var wind = {};
+wind.AddWindDetail = function () {
+    var wind = {};
+    wind.ApplicantType = $('.ApplicantType').find('.selected').find('.label').text();
+    wind.pincode = $("#txtPincode").val();
+    wind.address = $("#txtAddress").val();
+    wind.GPSLatitude = $("#txtGPSLatitude").val();
+    wind.GPSLongitude = $("#txtGPSLongitude").val();
+    wind.ApplicantName = $("#txtApplicantName").val();
+    wind.pan = $("#txtPAN").val();
+    wind.cin = $("#txtCIN").val();
+    wind.TotalWEGPlanned = $("#txtTotalWEGPlanned").val();
+    wind.ProposedCapacity = $("#txtProposedCapacity").val();
+    wind.EstimatedProjectCost = $("#txtEstimatedProjectCost").val();
+    wind.AvgLast3yrTurnOver = $("#txtAvgLast3yrTurnOver").val();
+    wind.name = $("#txtName").val();
+    wind.mobileNumber = $("#txtMobileNo").val();
+    wind.email = $("#txtEmailEnquiry2").val();
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        type: "POST",
+        url: "/Wind/Add",
+        async: false,
+        data: JSON.stringify(wind),
+        success: function (data) {
+            var status = data;
+            if (status) {
+                //window.location.href = "Enquiry/ThankYou";
+                alert("Success");
+            } else { }
+        }
+    });
+}
+
+$(document).ready(function () {
+    $('#btnWindSubmit').off("click").on("click", function () { wind.AddWindDetail(); });
+});
